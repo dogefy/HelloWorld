4b59e452809dcad3f956a9d3fc11649e09b2bdd5
dogefy <81802833+dogefy@users.noreply.github.com>
2022-01-11 10:21:53 +0800

    add

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
-f=open('log.txt','w')
-a=time.localtime()
-a=time.asctime(a)
+
+f = open('log.txt', 'w')
+a = time.localtime()
+a = time.asctime(a)
 f.write(str(a))
+f.write('hahaha')
 f.close()
